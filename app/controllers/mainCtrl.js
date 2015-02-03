f3b2cc701700dc5d6fc719ef2569b01c44ccfc60
Nathan <simon.nath@icloud.com>
2015-02-03 01:08:18 +0100

    Changements mineurs

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,5 +1,12 @@
 var dapxApp = angular.module('dapxApp');
 
-dapxApp.controller('mainCtrl', ['$scope','$http', function($scope, $http) {
-    $scope.user = "Nathan";
+dapxApp.controller('mainCtrl', ['$scope','$http','$location','subFactory', function($scope, $http, $location, subFactory) {
+    $scope.loading = true;
+    $scope.posts = subFactory.getPosts('all').then(function(posts){
+    	$scope.loading = false;
+
+    	$scope.posts = posts;
+    }, function(msg){
+    	alert(msg);
+    });
 }]);
--- app/controllers/min/mainCtrl-min.js
+++ app/controllers/min/mainCtrl-min.js
@@ -1,1 +1,1 @@
-var dapxApp=angular.module("dapxApp");dapxApp.controller("mainCtrl",["$scope","$http",function(p,a){p.user="Nathan"}]);
+var dapxApp=angular.module("dapxApp");dapxApp.controller("mainCtrl",["$scope","$http","$location","subFactory",function(o,t,a,n){o.loading=!0,o.posts=n.getPosts("all").then(function(t){o.loading=!1,o.posts=t},function(o){alert(o)})}]);
